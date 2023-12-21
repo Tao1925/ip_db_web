e6473fc5d060dfe245b3669a3bf77e850e746a1a
Tao1925 <yhxxtsy@163.com>
2023-12-21 09:09:41 +0800

    23.12.20

--- src/study/flask/flask01.py
+++ src/study/flask/flask01.py
@@ -1,21 +1,11 @@
-
 from flask import Flask
-from markupsafe import escape
 
-app = Flask(__name__)
+app = Flask(__name__) # 创建一个flask的实例
 
-# 1. basic
-# 
-# browser -> http://127.0.0.1:5000
-@app.route('/')
-def index():
-    return 'Index Page'
+# 这样加注解将会在所有访问该网址的时候，无论后面跟着的路径是什么，都返回这个hello字符串
+@app.route("/")
+def home():
+    return "Hello! \n <h1>HELLO</h1>"
 
-# 2. 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-
-@app.route('/<name>')
-def call(name):
-    return f"Hello, {escape(name)}!"
+if __name__ == "__main__":
+    app.run()
